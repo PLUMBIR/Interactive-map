8a1dd42aebe6238ecbdbf3ca7ee7ac0eaa9d7f4c
vbandarovich <worldvb2000@gmail.com>
2023-12-26 22:57:09 +0300

    Fixed style library imports, added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
+import {MdbCheckboxModule} from "mdb-angular-ui-kit/checkbox";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
-    HeaderModule
+    CommonModule,
+    MdbCheckboxModule,
+    HeaderModule,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  title = 'Interactive-map';
+  title = 'Interactive map';
 }
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent {
+
+}
