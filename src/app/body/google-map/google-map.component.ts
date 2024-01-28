cd47109b11c5f825bd0b34e290dd97413b8ee8a3
PLUMBIR <bondarovich.kirya@gmail.com>
2024-01-28 17:45:07 +0300

    Added options for google map.

--- src/app/body/google-map/google-map.component.ts
+++ src/app/body/google-map/google-map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {GoogleMapsModule} from "@angular/google-maps";
 
@@ -13,5 +13,33 @@ import {GoogleMapsModule} from "@angular/google-maps";
   styleUrls: ['./google-map.component.scss']
 })
 export class GoogleMapComponent {
+  zoom = 7.1;
+  center: google.maps.LatLngLiteral = {
+    lat: 53.7098,
+    lng: 27.9534,
+  };
 
+  bounds: google.maps.LatLngBoundsLiteral = {
+    north: 56.1655,
+    south: 51.2565,
+    west: 23.1785,
+    east: 32.7209,
+  };
+
+  options: google.maps.MapOptions = {
+    disableDoubleClickZoom: true,
+    fullscreenControl: false,
+    zoomControl: false,
+    keyboardShortcuts: false,
+    clickableIcons: false,
+    mapTypeControl: false,
+    restriction: {
+      latLngBounds: this.bounds,
+      strictBounds: false,
+    },
+    scrollwheel: true,
+    minZoom: 7.1,
+  };
 }
+
+
